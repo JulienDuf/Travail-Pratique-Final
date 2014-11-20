7519430729abdcd85b2deae011b19df3d261792f
Samuel Rambaud <sami.rami@hotmail.com>
2014-11-20 15:45:38 -0500

    Fin des constructeur et affichage
    constructeurs et affichage des packs terminées

--- Travail Final/Travail Final/CHealthPack.h
+++ Travail Final/Travail Final/CHealthPack.h
@@ -7,19 +7,33 @@ private:
 	SDL_Rect m_pPosition; //Position du pack sur la map
 	SDL_Texture* m_pTexture; //Texture du pack
 public:
-	CHealthPack(const char* _Destination, SDL_Renderer* _Renderer){
-		m_pTexture IMG_LoadTexture(_Renderer, _Destination + "\\Armes et Packs\\HealthPack.jpg");
-		m_pPosition.h = 100;
-		m_pPosition.w = 100;
-		
+	//constructeur
+	CHealthPack(string* _SourceImage, SDL_Renderer* _Renderer){
+		//initialisation de la texture
+		_SourceImage->append("\\Armes et Packs\\HealthPack.png");
+		m_pTexture = IMG_LoadTexture(_Renderer, _SourceImage->c_str());
+
+		//creation de la position du pack
+		m_pPosition.h = 40;
+		m_pPosition.w = 34;
+		m_pPosition.y = 0;
+		m_pPosition.x = rand() % 1366;
 	}
 
-	void Use(CPlayer* _Player){
-		_Player->SetHealth(_Player->GetHealth() + ((_Player->GetHealth())*0.25));
-	}
+	/*
+	Utilisation du pack de vie
+	parametre:
+		_player: Joueur utilisant le pack.
+	*/
+	//void Use(CPlayer* _Player){
+	//	_Player->SetHealth(_Player->GetHealth() + ((_Player->GetHealth())*0.25));
+	//}
 
+	/*
+	Affichage du pack de vie sur la map a la position m_pPosition
+	*/
 	void Show(SDL_Renderer* _Renderer){
-
+		SDL_RenderCopy(_Renderer, m_pTexture, NULL, new SDL_Rect(m_pPosition));
 	}
 
 };
--- Travail Final/Travail Final/CMine.h
+++ Travail Final/Travail Final/CMine.h
@@ -4,16 +4,38 @@
 class CMine : public CPack{
 private:
 	SDL_Rect m_pPosition; //position de la mine sur la map
+	SDL_Texture* m_pTexture; //Texture du pack
+
 public:
-	CMine(){
+	CMine(string* _SourceImage, SDL_Renderer* _Renderer){
+		//initialisation de la texture.
+		_SourceImage->append("\\Armes et Packs\\Mine.png");
+		m_pTexture = IMG_LoadTexture(_Renderer, _SourceImage->c_str());
 
+		//creation de la position de la mine
+		m_pPosition.h = 59;
+		m_pPosition.w = 15;
+		m_pPosition.y = 0;
+		m_pPosition.x = rand() % 1366;
 	}
 
-	void Use(CPlayer* _Player){
+	/*void Use(CPlayer* _Player){
 		_Player->SetHealth((_Player->GetHealth())*0.25);
+	}*/
+
+
+	/*
+	Affiche la mine sur la  map a la postion m_pPosition
+	*/
+	void Show(SDL_Renderer* _Renderer){
+		SDL_RenderCopy(_Renderer, m_pTexture, NULL, new SDL_Rect(m_pPosition));
 	}
 
-	void Show(){
-
+	
+	/*
+	Calcule la physique gerant la chute de la mine lors de son apparition.
+	*/
+	void PhysiqueChutePack(){
+		m_pPosition.y += m_pPosition.y
 	}
 };

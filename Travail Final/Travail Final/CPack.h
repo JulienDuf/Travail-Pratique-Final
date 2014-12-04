88651335c6d5f61b44e5ccaaa3bb1000bee411f6
Julien Dufresne <dufresne_julien@hotmail.ca>
2014-12-04 10:52:39 -0500

    Ajustement de code de deux branche

--- Travail Final/Travail Final/CPack.h
+++ Travail Final/Travail Final/CPack.h
@@ -6,4 +6,10 @@ public:
 
 	virtual void ShowPack(SDL_Renderer* _pSDLRenderer) = 0;
 
+	virtual void Use(void) = 0;
+
+	virtual void GetSurface(void) = 0;
+
+	virtual void GetRectDestination(void) = 0;
+
 };
